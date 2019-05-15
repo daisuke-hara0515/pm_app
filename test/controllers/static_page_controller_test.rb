94afe716ad809e1cebd381e0079973b5f189656a
daisuke-hara0515 <wildlife402@gmail.com>
2019-05-16 07:47:56 +0900

    Home、About、Helpページのタイトルタグ内の特定文字が存在するかどうかのテストコード追加

--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -6,15 +6,19 @@ class StaticPageControllerTest < ActionDispatch::IntegrationTest
     #Getリクエストをstaticpageコントローラーのhomeアクションに対して発行
     assert_response :success
     #リクエストに対するレスポンスは成功するはず
+    assert_select "title","Home | PocketMoney App"
+    #テンプレート内のタイトルタグにHome | PocketMoney Appが存在するか
   end
 
   test "should get help" do
     get static_page_help_url
     assert_response :success
+    assert_select "title","Help | PocketMoney App"
   end
 
   test "should get about" do
     get static_page_about_url
     assert_response :success
+    assert_select "title","About | PocketMoney App"
   end
 end
