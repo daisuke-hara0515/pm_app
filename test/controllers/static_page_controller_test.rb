40f1eea1557663173905049a76c1634f9fb9b816
daisuke-hara0515 <wildlife402@gmail.com>
2019-05-17 23:16:57 +0900

    インスタンス変数を使ってリファクタリング

--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -1,24 +1,29 @@
 require 'test_helper'
 
 class StaticPageControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "PocketMoney App"
+  end
+
   test "should get home" do #homeページのテスト
     get static_page_home_url 
     #Getリクエストをstaticpageコントローラーのhomeアクションに対して発行
     assert_response :success
     #リクエストに対するレスポンスは成功するはず
-    assert_select "title","Home | PocketMoney App"
+    assert_select "title","Home | #{@base_title}"
     #テンプレート内のタイトルタグにHome | PocketMoney Appが存在するか
   end
 
   test "should get help" do
     get static_page_help_url
     assert_response :success
-    assert_select "title","Help | PocketMoney App"
+    assert_select "title","Help | #{@base_title}"
   end
 
   test "should get about" do
     get static_page_about_url
     assert_response :success
-    assert_select "title","About | PocketMoney App"
+    assert_select "title","About | #{@base_title}"
   end
 end
