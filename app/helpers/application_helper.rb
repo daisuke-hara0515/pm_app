4292388e93d07988b1af81308231fe197aec2a22
daisuke-hara0515 <wildlife402@gmail.com>
2019-05-21 20:20:41 +0900

    viewに直接コードを書き込まずに、application.html.erbとapplicationヘルパーにコードを記述することで動的なタイトルを表示することができる

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    #ページ毎の完全なタイトルを返します。
+    def full_title(page_title = '')
+        base_title = "PocketMoney App" #base_titleが"PocketMoney App"という文字が定義されている
+        if page_title.empty? #full_titleメソッドの引数が空白かどうか
+            base_title
+        else
+            page_title + "|" +base_title
+        end
+    end
 end
--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -16,19 +16,19 @@ class StaticPageControllerTest < ActionDispatch::IntegrationTest
     #Getリクエストをstaticpageコントローラーのhomeアクションに対して発行
     assert_response :success
     #リクエストに対するレスポンスは成功するはず
-    assert_select "title","Home | #{@base_title}"
-    #テンプレート内のタイトルタグにHome | PocketMoney Appが存在するか
+    assert_select "title","PocketMoney App"
+    #テンプレート内のタイトルタグにPocketMoney Appが存在するか
   end
 
   test "should get help" do
     get static_page_help_url
     assert_response :success
-    assert_select "title","Help | #{@base_title}"
+    assert_select "title","Help|#{@base_title}"
   end
 
   test "should get about" do
     get static_page_about_url
     assert_response :success
-    assert_select "title","About | #{@base_title}"
+    assert_select "title","About|#{@base_title}"
   end
 end
