2b4bd494b7eb0adecdfe1fd5756b1af518e85485
daisuke-hara0515 <wildlife402@gmail.com>
2019-05-26 12:43:22 +0900

    名前付きルート作成準備

--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -31,4 +31,10 @@ class StaticPageControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title","About|#{@base_title}"
   end
+
+  test "should get contact" do
+    get static_page_contact_url
+    assert_response :success
+    assert_select "title","Contact|#{@base_title}"
+  end
 end
