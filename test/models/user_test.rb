6636668e1e319f9015ed752446e5f939eb16a46a
daisuke-hara0515 <wildlife402@gmail.com>
2019-06-02 12:55:13 +0900

    nameとemailが空白ではない時はUserモデルが有効であることを確認するテストコードを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,6 +6,16 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "should be valid" do
-    assert @user.valid?
+    assert @user.valid? #@userが有効かどうかのテスト
+  end
+
+  test "name should be present" do
+    @user.name = "" #@userのnameを空白にする
+    assert_not @user.valid? #@userが有効では無いかどうかのテスト
+  end
+
+  test "email should be present" do
+    @user.email = " " #@userのemailが空白
+    assert_not @user.valid?  #@userが有効では無いことをテスト
   end
 end
