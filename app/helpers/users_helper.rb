37199d357d04ffecae4235872091fba51744ab03
daisuke-hara0515 <wildlife402@gmail.com>
2019-06-17 22:10:49 +0900

    プロフィール画像は後ほどリファクタリング予定

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+    #引数で与えられたユーザーのGravatar画像を返す
+    def gravatar_for(user,size: 80)
+        #DigestライブラリのhexdigestメソッドでMD5を使ってハッシュ
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        #gravatar_idの値が代入され、そのURLがgravatar_url
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}? s=#{size}"
+        image_tag(gravatar_url,alt: user.name, class: "gravatar")
+    end
 end
