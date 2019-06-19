f036147ed6fb8d77538b05fa98fdd6eab5800fb8
daisuke-hara0515 <wildlife402@gmail.com>
2019-06-19 21:58:18 +0900

    createメソッドを追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,13 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      #if @user.saveがtrueであるかどうか。falseならnewページをrenderする
+    else
+      render 'new'
+    end
+  end
 end
