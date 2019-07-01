8cc05ac90ec315d73cddff1840484491d0dcb235
daisuke-hara0515 <wildlife402@gmail.com>
2019-07-01 19:57:38 +0900

    ユーザー登録成功したらユーザーページへredirectする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      redirect_to user_url(@user) #redirect_to user_url(@user)と等価のコード
       #if @user.saveがtrueであるかどうか。falseならnewページをrenderする
     else
       render 'new'
