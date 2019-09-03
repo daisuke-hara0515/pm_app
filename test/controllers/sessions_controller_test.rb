eb4f8bdd304a4b46409699bca84d80499c25f6aa
daisuke-hara0515 <wildlife402@gmail.com>
2019-09-03 20:35:39 +0900

    なぜかルーティングが抜けているので修正

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class SessionsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get login_path
-    assert_response :success
+    assert_response :success #:successはステータスコード200〜299を指定している（成功ステータス）
   end
 
 end
