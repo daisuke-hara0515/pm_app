11a4e5a28b53aa15f6f3719b02fb082e1f4d71d3
daisuke-hara0515 <wildlife402@gmail.com>
2019-09-28 14:35:52 +0900

    フレンドリーフォワーディング追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,10 @@ module SessionsHelper
         cookies.permanent[:remember_token] = user.remember_token
     end
 
+    def current_user?(user)
+        user == current_user
+    end
+
     #記憶トークンcookieに対応するユーザーを返す
     def current_user
         if (user_id = session[:user_id]) #user_idにuser_idのセッションを代入して存在すれば・・・
@@ -38,5 +42,17 @@ module SessionsHelper
         forget(current_user)
         session.delete(:user_id)
         @current_user = nil
+    end
+
+    #記憶したURL（もしくはデフォルト値）にリダイレクト
+    def redirect_back_or(default)
+        redirect_to(session[:forwarding_url] || default)
+        session.delete(:forwarding_url)
+    end
+
+    #アクセスしようとしたURLを覚えておく
+    #requestをgetする時だけ転送先URLを覚えるようにしておく(if request.get?)
+    def store_location
+        session[:forwarding_url] = request.original_url if request.get?
     end
 end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -34,4 +34,23 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal name, @user.name
     assert_equal email, @user.email
   end
+
+  #michaelで編集しようとする⇨ログイン促される⇨ログイン⇨編集⇨Foobarとか入力⇨保存⇨更新⇨OKとなる
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    assert_equal session[:forwarding_url], edit_user_url(@user)
+    log_in_as(@user)
+    assert_nil session[:forwarding_url]
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user),params: {user:{name: name,
+                          email: email,
+                          password: "",
+                          password_confirmation: ""}}
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
 end
