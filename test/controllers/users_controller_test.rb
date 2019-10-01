8d99616c4f5ab6c875f7a93ca54badf17229d250
daisuke-hara0515 <wildlife402@gmail.com>
2019-10-01 09:07:41 +0900

    adminユーザーのテスト

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -33,6 +33,15 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user),params: {
+                     user: {password: @other_user.password,
+                           password_confirmation: @other_user.password,
+                           admin: true }}
+    assert_not @other_user.reload.admin?
+  end
   #他のユーザーが自分のユーザーを編集できないかを確認するテスト
   test "should redirect edit when logged in as wrong user" do
     log_in_as(@other_user)
@@ -48,5 +57,4 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert flash.empty?
     assert_redirected_to root_url
   end
-
 end
