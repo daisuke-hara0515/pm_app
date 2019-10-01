0f49d24cef15a109b4e584a3620db4b0f641b29a
daisuke-hara0515 <wildlife402@gmail.com>
2019-10-01 20:46:43 +0900

    ユーザー削除テスト追加

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,16 +3,30 @@ require 'test_helper'
 class UsersIndexTest < ActionDispatch::IntegrationTest
   
   def setup
-    @user = users(:michael)
+    @admin     = users(:michael)
+    @non_admin = users(:archer)
   end
 
-  test "index including pagenation" do
-    log_in_as(@user)
+  test "index as admin including pagination and delete links" do
+    log_in_as(@admin)
     get users_path
-    assert_template 'users/index' #users/indexテンプレートが選択されているか
-    assert_select 'div.pagination',count:2 #<div class="pagination">foobar</div>にマッチするものがあるか
-    User.paginate(page: 1).each do |user| #User.paginate(page:1)の戻り値に対してeachメソッド
-      assert_select 'a[href=?]',user_path(user),text: user.name #<a href="users/User_id">user.name</a>にマッチするものがあるか
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end
     end
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
+    end
+  end
+
+  test "index as non-admin" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
   end
 end
