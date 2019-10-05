2bbc11739f6cc5703f0219003b9a0c3daa9b17de
daisuke-hara0515 <wildlife402@gmail.com>
2019-10-05 14:33:56 +0900

    保存忘れ

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ApplicationMailer
 
-  def account_activation(user)
+  def account_activation(user) #アクティベート化メールアクション
     @user = user
     mail to: user.email, subject: "Account activation"
   end
 
-  def password_reset
+  def password_reset #パスワードリセットアクション
     @greeting = "Hi"
 
     mail to: "to@example.org"
